5b77e66f7a911ddb3b0fb8cb44bd7b6813e08002
alex broder <alexbroder119@gmail.com>
2020-12-01 20:23:28 +0200

    quick update on java script
    added the confirm before the user can add tasks ,changed the functions to arrow functions added a bit spacing where the tasks should appear

--- to-do-app.js
+++ to-do-app.js
@@ -4,34 +4,41 @@ let input = document.getElementById('taskinput');
 let list = document.getElementById("myList");
 
 
-btn1.addEventListener('click', function () {
+btn1.addEventListener('click', () => {
   if (input.value == '') {
     alert('need to be at least 1 letter or number')
   } else {
-    let li = document.createElement('li');
-    let del = document.createElement('button');
-    let doneBtn = document.createElement('button');
-    doneBtn.innerHTML = 'Finished?';
-    doneBtn.setAttribute('class', 'btn btn-outline-success');
-    doneBtn.style.position = 'relative';
-    doneBtn.style.left = `5px`;
-    del.setAttribute('class', 'btn btn-outline-danger');
-    del.innerHTML = 'Delete';
-    list.appendChild(li);
-    li.innerHTML += input.value + "<br/> ";
-    li.appendChild(del);
-    li.appendChild(doneBtn);
-    del.addEventListener('click', function () {
-      del.parentElement.remove();
-    })
-    doneBtn.addEventListener('click', function () {
-      doneBtn.innerHTML = "Task is Done!";
-      doneBtn.setAttribute('disabled', 'disabled');
-    })
+    if (confirm('are you sure you want to add this task?') == true) {
 
+
+      let li = document.createElement('li');
+      li.style.margin = '5px';
+      let del = document.createElement('button');
+      let doneBtn = document.createElement('button');
+      doneBtn.innerHTML = 'Finished?';
+      doneBtn.setAttribute('class', 'btn btn-outline-success');
+      doneBtn.style.position = 'relative';
+      doneBtn.style.left = `25px`;
+      del.setAttribute('class', 'btn btn-outline-danger');
+      del.style.position = 'relative';
+      del.style.left = `20px`;
+      del.innerHTML = 'Delete';
+      list.appendChild(li);
+      li.innerHTML += input.value;
+      li.appendChild(del);
+      li.appendChild(doneBtn);
+      del.addEventListener('click', () => {
+        del.parentElement.remove();
+      })
+      doneBtn.addEventListener('click', () => {
+        doneBtn.innerHTML = "Task is Done!";
+        doneBtn.setAttribute('disabled', 'disabled');
+      })
+
+    }
   }
 })
-btn2.addEventListener('click', function () {
+btn2.addEventListener('click', () => {
   list.innerHTML = '';
 })
 
